98596a28d8a473365ed0e8b88e66a3c0d8240336
Ashtonval <tennisjedi20xx@gmail.com>
2022-10-26 09:05:07 -0700

    Added our project
    Added our scripting project

---
+++ ScriptingProjectKumar/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int myInt = 5;
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
